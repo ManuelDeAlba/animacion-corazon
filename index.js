a80e934c7e7fa1caea43452c4da399cf81b65545
Manuel <ferman5903@hotmail.com>
2022-12-20 12:49:46 -0600

    Primer commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const socketIO = require("socket.io");
+const app = express();
+
+app.set("PORT", process.env.PORT || 3000);
+
+app.use(express.static("public"));
+
+const server = app.listen(app.get("PORT"), () => {
+    console.log("Servidor en el puerto", app.get("PORT"));
+})
+
+const io = socketIO(server);
+
+io.on('connection', socket => {
+    console.log("Usuario conectado:", socket.id);
+
+    socket.on("palpitar", () => {
+        io.emit("palpitar");
+    });
+});
+  
